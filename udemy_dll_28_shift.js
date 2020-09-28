dd6a55771a5fba5379780e3517e5788c9bd2a67e
Crew Spence <csspence123@gmail.com>
2020-09-28 05:43:48 -0500

    Created function stub for toy problem.

---
+++ udemy_dll_28_shift.js
@@ -0,0 +1,40 @@
+/*
+DLL Shift Exercise - JavaScript Algorithms and Data Structures Masterclass
+
+Implement the following on a DoublyLinkedList class
+
+shift 
+
+This function should remove a node at the beginning of a DLL.  It should return the node removed.
+*/
+
+class Node{
+    constructor(val){
+        this.val = val
+        this.next = null;      
+        this.prev = null;      
+    }
+}
+
+class DoublyLinkedList{
+    constructor(val){
+        this.val = val
+        this.next = null;      
+    }
+    push(val){
+        var node = new Node(val);
+        if (this.head === null) {
+            this.head = node;
+            this.tail = this.head;
+        } else {
+            this.tail.next = node;
+            node.prev = this.tail;
+            this.tail = node;
+        }
+        this.length++;
+        return this;
+    }
+    shift(){
+        
+    }
+}
